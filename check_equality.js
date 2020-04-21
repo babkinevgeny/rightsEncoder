bbd7e05e7db4b51425dec596990510e48db2ae95
babkinevgeny <babkinevgeny@gmail.com>
2020-04-21 14:54:36 +0300

    Removed checking via lodash.isequal and added my own way of checking.

--- check_equality.js
+++ check_equality.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
-const isEqual = require('lodash.isequal');
 
 const beforeEncoding = fs.readFileSync('./rights.json');
 const afterDecoding = fs.readFileSync('./decoded_rights.json');
+const bEp = JSON.parse(beforeEncoding).view;
+const aDp = JSON.parse(afterDecoding).view;
+const arr = [];
 
-console.log(isEqual(beforeEncoding, afterDecoding));
+bEp.forEach(key => {
+    return !aDp.includes(key) ? arr.push(key) : null
+})
+
+if (arr.length) {
+    console.log(`Rights before encoding IS NOT equal rigths after decoding. Here is the difference: ${arr}`)
+} else {
+    console.log('Rights after decoding contents all original rights!')
+}
