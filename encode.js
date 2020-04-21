492c8d5bebcd70f0fb9a74f0240965fa7d1c61c2
babkinevgeny <babkinevgeny@gmail.com>
2020-04-21 17:26:48 +0300

    Got rid of extra special fields. Added big comment about logic.

--- encode.js
+++ encode.js
@@ -2,34 +2,55 @@ const fs = require('fs');
 let jsonData = fs.readFileSync('./rights.json');
 let obj = JSON.parse(jsonData);
 
+/* 
+    Случаи, когда текущий путь является подстрокой последующих путей
+    (напр., "OPERATION_AUTO.columns","OPERATION_AUTO.columns.ACTUAL_COST_BUY"), назовем их "особенными", 
+    должны быть учтены отдельно. Для таких случаев в объект будет добавлено поле "_": 1, 
+    при встрече которого функция decodeRights будет понимать, что этот путь необходимо выделить в отдельный элемент массива.
+
+    В нашей БД права хранятся в порядке от общего к частному:
+    [
+        "OPERATION_AUTO.columns",
+        "OPERATION_AUTO.columns.MIN_GTD_DATE_ADD_UNITS",
+        "OPERATION_AUTO.columns.ACTUAL_COST_BUY",
+        "OPERATION_AUTO.columns.ACTUAL_COST_SALE"...
+    ]
+
+    Порядок прав в массиве важен, тк функция encodeRights не рассматривает случаи, когда "особенные" поля находятся в конце массива.
+*/
+
 const encodeRights = (paths) => {
     let obj = {};
 
-    const createObj = (currentObj, keys) => {
+    const createObj = (currentObj, keys, needsCutting = false) => {
         const key = keys.shift();
 
         if (!currentObj.hasOwnProperty(key)) { // если такого ключа нет, то добавляем пустой объект.
             currentObj[key] = {}
         }
 
-        if (keys.length === 0) { // если это последнее поле из массива
-            const hasKeys = Object.keys(currentObj[key]).length ? true : false; // проверяем пустой ли объект
-            if (hasKeys) {
-                currentObj[key] = {
-                    _: 1,
-                    ...currentObj[key]
-                };
-            } else {
-                currentObj[key] = {_: 1};
+        if (keys.length === 0 && needsCutting) { // если "особый" случай, то добавляем специальное поле
+            currentObj[key] = {
+                "_": 1,
+                ...currentObj[key]
             }
         }
 
-        return keys.length ? createObj(currentObj[key], keys) : null;
+        return keys.length ? createObj(currentObj[key], keys, needsCutting) : null;
     }
 
-    paths.forEach(path => {
+    paths.forEach((path, index, paths) => {
         const keys = path.split(".");
-        createObj(obj, keys);
+
+        const isLastPath = index === paths.length - 1;
+       
+        // проверяем является ли текущий путь последним и содержит ли следующий путь текущий
+        if ( !isLastPath && paths[index + 1].includes(`${path}.`)) { // "OPERATION.tabs.OPERATION_UNIT".includes("OPERATION.tabs.OPERATION") вернет true, поэтому к path добавим точку
+            createObj(obj, keys, true); // помечаем, что случай особый (needsCutting = true)
+        } else {
+            createObj(obj, keys);
+        }
+        
     })
 
     return obj;
