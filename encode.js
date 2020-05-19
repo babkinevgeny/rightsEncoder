3eb1a8fe761fe44d0d0da19acf185cb2dd376df9
babkinevgeny <babkinevgeny@gmail.com>
2020-05-19 13:05:45 +0300

    Returned to nested object, but without sorting

--- encode.js
+++ encode.js
@@ -5,25 +5,18 @@ const fs = require('fs');
     (напр., "OPERATION_AUTO.columns","OPERATION_AUTO.columns.ACTUAL_COST_BUY"), назовем их "особенными", 
     должны быть учтены отдельно. Для таких случаев в объект будет добавлено поле "_": 1, 
     при встрече которого функция decodeRights будет понимать, что этот путь необходимо выделить в отдельный элемент массива.
-
-    В нашей БД права хранятся в порядке от общего к частному:
-    [
-        "OPERATION_AUTO.columns",
-        "OPERATION_AUTO.columns.MIN_GTD_DATE_ADD_UNITS",
-        "OPERATION_AUTO.columns.ACTUAL_COST_BUY",
-        "OPERATION_AUTO.columns.ACTUAL_COST_SALE"...
-    ]
-
-    Порядок прав в массиве важен, тк функция encodeRights не рассматривает случаи, когда "особенные" поля находятся в конце массива.
 */
 
 let i = 0
 
 const encodeRights = (paths) => {
     let obj = {};
-    const createObj = (currentObj, keys, needsCutting = false) => {
-        const key = keys.shift();
-        fs.appendFileSync('./logs.txt', `${i++} ${key}\n`)
+    const createObj = (currentObj, keys, index = 0, needsCutting = false) => {
+        const key = keys[index];
+
+        if ( !key ) {
+            return;
+        }
 
         const hasKey = currentObj.hasOwnProperty(key);
 
@@ -31,37 +24,44 @@ const encodeRights = (paths) => {
             currentObj[key] = {};
         }
 
-        if ( keys.length === 0 && needsCutting ) { // если "особый" случай, то добавляем специальное поле
+        const isLast = index === keys.length - 1;
+
+        if ( needsCutting && isLast ) { // если "особый" случай, то добавляем специальное поле
             currentObj[key] = {
                 _: 1,
                 ...currentObj[key]
             };
         }
 
-        if ( keys.length ) {
-            return createObj(currentObj[key], keys, needsCutting);
+        if ( index < keys.length ) {
+            return createObj(currentObj[key], keys, index + 1, needsCutting);
         }
-        
+
         return;
     };
 
-    if (paths) {
-        paths.sort();
+    if ( paths ) {
         paths.forEach((path, index, paths) => {
-            fs.appendFileSync('./logs.txt', `${path}\n`);
             const keys = path.split(".");
-    
+
             const isLastPath = index === paths.length - 1;
+
             const nextPath = paths[index + 1];
+            const prevPath = paths[index - 1];
+
             const nextPathIncludesCurrent = nextPath ? nextPath.includes(`${path}.`) : null; // "OPERATION.tabs.OPERATION_UNIT".includes("OPERATION.tabs.OPERATION") вернет true, поэтому к path добавим точку
-    
-            // проверяем является ли текущий путь последним и содержится ли текущий путь в следующем пути
-            if ( !isLastPath && nextPathIncludesCurrent ) {
-                createObj(obj, keys, true); // помечаем, что случай особый (needsCutting = true)
+            const prevPathIncludesCurrent = prevPath ? prevPath.includes(`${path}.`) : null; // "OPERATION.tabs.OPERATION_UNIT".includes("OPERATION.tabs.OPERATION") вернет true, поэтому к path добавим точку
+
+            if (nextPathIncludesCurrent || prevPathIncludesCurrent) {
+                createObj(obj, keys, 0, true);
             } else {
-                createObj(obj, keys);
+                createObj(obj, keys, 0);
             }
-    
+
+            if (isLastPath && prevPathIncludesCurrent) {
+                createObj(obj, keys, 0, true);
+            }
+            
         });
     }
 
